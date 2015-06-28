374dfb834eb2dca9e628ba851d28c7e5e4a96dc7
Sascha Schimke <sascha.schimke@postcon.de>
2015-06-28 18:59:36 +0200

    small fibonacci test case

--- tests/IteratorStreamTest.php
+++ tests/IteratorStreamTest.php
@@ -7,6 +7,13 @@ use Sci\Stream\Stream;
 
 class IteratorStreamTest extends AbstractStreamTest
 {
+    public function testFibonacci()
+    {
+        $stream = new IteratorStream($this->fibonacci(1000));
+
+        $this->assertEquals([1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987], $stream->toArray());
+    }
+
     /**
      * returns the system under test
      *
@@ -14,9 +21,23 @@ class IteratorStreamTest extends AbstractStreamTest
      */
     protected function getSut()
     {
-        $array = range(0, 99);
+        $array    = range(0, 99);
         $iterator = new \ArrayIterator($array);
 
         return IteratorStream::create($iterator);
+    }
+
+    /**
+     * Generates the fibonacci sequence up to $max
+     *
+     * @param int $max
+     *
+     * @return \Generator
+     */
+    private function fibonacci($max)
+    {
+        for ($x = 0, $y = 1; $y <= $max; $z = $x + $y, $x = $y, $y = $z) {
+            yield $y;
+        }
     }
 }
